6c59921746fcc044e160f402ffa2e36b9bef22e6
MohammedHW <hoshsangabadwalam@gmail.com>
2026-09-26 14:40:26 +0530

    feat(wallet): direct loan cards to respective finance flows

--- lib/page/wallet/widgets/wallet_overview_tab.dart
+++ lib/page/wallet/widgets/wallet_overview_tab.dart
@@ -623,12 +623,20 @@ class WalletOverviewTab extends StatelessWidget {
         'title': title,
         'amount': amount,
         'name': fullName,
+        'phone': mobile,
         'mobile': mobile,
         'pocket_number': pocketNumber,
         'user_type': 'customer',
       };
 
-      final uri = Uri.parse("https://api.fiinway.com/finance")
+      String subPath = '';
+      if (cardType == '0 CIBIL Loan' || cardType == 'Interest Free Loan') {
+        subPath = '/zero-cibil/intro';
+      } else if (cardType == 'Low CIBIL Loan') {
+        subPath = '/cash-loan/apply';
+      }
+
+      final uri = Uri.parse("https://api.fiinway.com/finance$subPath")
           .replace(queryParameters: queryParams);
 
       Get.to(() => WebViewScreen(
